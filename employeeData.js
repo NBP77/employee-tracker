d04aaafb8a5f1244b7362ec25b8d2608d837e8df
NBP77 <nbp77@outlook.com>
2021-04-07 15:46:39 -0400

    Getting boilerplate and intalling packages

---
+++ employeeData.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  port: 3306,
+
+  user: 'root',
+
+  password: 'Pendonevo13!',
+  database: 'ice_creamDB',
+});
+
+const afterConnection = () => {
+  connection.query('SELECT * FROM products', (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+};
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  afterConnection();
+});
