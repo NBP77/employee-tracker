46409d4062a926327ecaaa6d73ffb30434499edb
NBP77 <nbp77@outlook.com>
2021-04-07 20:59:12 -0400

    Created inquirer prompts, questions, begining function to run the prompts and suedo codded the functions for the anwsers

--- employeeData.js
+++ employeeData.js
@@ -3,25 +3,56 @@ const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-
   port: 3306,
-
   user: 'root',
-
   password: '',
   database: 'employee_trackerDB',
 });
 
-const afterConnection = () => {
-  connection.query('SELECT * FROM employee', (err, res) => {
-    if (err) throw err;
-    console.table(res);
-    connection.end();
-  });
-};
+
+// Function for prompts 
+const begin = () => {
+    inquirer.prompt(prompts.startPrompts)
+    // .then?
+}
+
+const startPrompts = [
+    {
+        type: 'list',
+        name: 'options',
+        message: 'Please select an option below',
+        choices: [
+            "Add departments, roles, and/or employees",
+            "View departments, roles, employees",
+            "Update employee roles",
+            "Exit"
+        ]
+    }
+]
+
+// Functions for options 
+
+// Add departments, roles, and/or employees
+
+// View departments, roles, employees
+
+// Update employee roles
+
+// Exit
+
 
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}`);
-  afterConnection();
+  begin();
 });
+
+
+
+// const afterConnection = () => {
+//   connection.query('SELECT * FROM employee', (err, res) => {
+//     if (err) throw err;
+//     console.table(res);
+//     connection.end();
+//   });
+// };
